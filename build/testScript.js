691bc17335eae93ea01e4f5ab3b068fae48a4b08
Iulia Ryseva <yuliya.ryseva@ligastavok.ru>
2023-08-15 01:20:40 +0300

    upd build

---
+++ build/testScript.js
@@ -0,0 +1,12 @@
+let groundFloor;
+
+function setup() {
+    createCanvas(800, 400);
+    new Sprite();
+    // world.gravity.y = 10;
+    // groundFloor = new window.p5.Sprite(0, height - 30, width * 2, 20, 'static');
+}
+
+function draw() {
+    background(145, 125, 0);
+}
